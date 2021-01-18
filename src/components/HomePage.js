aa925bcf903b0d66d50f9c11de8f4aad22db0b9f
Daniel <cphangwei@student.usm.my>
2021-01-18 20:20:53 +0800

    perform merge on the branch due to previous commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-//page after user logs in
-
-import React from "react"
-import firebase from "firebase/app";
-import Top from './HomePageComponent/top-half'
-import NavBar from './NavBar'
-import Footer from './Footer'
-
-export default function HomePage() {
-//get the current user id
-const userID = firebase.auth().currentUser.uid
-sessionStorage.setItem("userID", userID)
-    return(
-        <>
-            <NavBar/>
-
-            <div>
-                <Top></Top>
-            </div>
-            <Footer/>
-        </>
-    );
-=======
 //page after user logs in
 
 import React from "react"
@@ -45,5 +21,4 @@ sessionStorage.setItem("userID", userID)
             <Footer/>
         </>
     );
->>>>>>> e88a7b6b0b2a4b891f123cb44a9dc18619ce0915
 }
